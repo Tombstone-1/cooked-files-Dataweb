1c96fddd11ab5087613ad38016a86f0664980463
Ashif hasan <asif.hasan1997vits@gmail.com>
2022-11-06 23:59:56 +0530

    removed secret key

--- Dserver/settings.py
+++ Dserver/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&oduz-b@5&!l&^2#tw8)bycwe(7xnh=17bvy6_yiq!gb!2%6pm'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
